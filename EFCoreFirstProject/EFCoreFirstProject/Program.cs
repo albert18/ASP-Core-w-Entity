dccc54bcfa1bea35ea21c4c297d8ec54b327f7dc
albert18 <caps18@hotmail.com>
2019-03-10 19:58:12 -0400

    section 1 - 1

--- EFCoreFirstProject/EFCoreFirstProject/Program.cs
+++ EFCoreFirstProject/EFCoreFirstProject/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace EFCoreFirstProject
 {
+    //POCO - Plain Old CLR Object
+    class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    class EFCoreQorganizationDb : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Department D = new Department() { DepartmentId = 1, Description = "Development", Name = "Dev" };
+
+            EFCoreQorganizationDb efCntx = new EFCoreQorganizationDb();
+            //Select all departments
+            List<Department> Depts = efCntx.Departments.ToList();
+
+            efCntx.Departments.Add(D);
+
+            efCntx.SaveChanges();
         }
     }
 }
