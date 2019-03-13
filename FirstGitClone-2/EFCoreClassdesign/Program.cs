358b57d3b9bb36de8109694f5dd88bb4bec20aa5
albert18 <caps18@hotmail.com>
2019-03-12 21:05:02 -0400

    core design

---
+++ FirstGitClone-2/EFCoreClassdesign/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreClassdesign
+{
+
+    class ClassStandard
+    {
+        [Key]
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string Description { get; set; }
+        public List<Student> Students{ get; set; }
+    }
+
+    class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Gender { get; set; }
+        public ClassStandard ClassStandard { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
