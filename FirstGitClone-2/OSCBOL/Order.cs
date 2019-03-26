9d36e2bc137ec24c3f567ee9321a06393a5e5944
albert18 <caps18@hotmail.com>
2019-03-25 20:43:55 -0400

    section 10

---
+++ FirstGitClone-2/OSCBOL/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OSCBOL
+{
+    [Table("Order")]
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
---
+++ FirstGitClone-2/OSCBOL/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OSCBOL
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [Required]
+        public double PricePerUnit { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
