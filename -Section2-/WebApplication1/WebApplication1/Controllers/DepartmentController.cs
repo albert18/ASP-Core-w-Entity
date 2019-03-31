826240eb1d8735becbad243dcdf900a5c45e627a
albert18 <caps18@hotmail.com>
2019-03-31 17:31:18 -0400

    update

---
+++ -Section2-/WebApplication1/WebApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    //Controller
+    public class DepartmentController : Controller
+    {
+        //Action
+        public string GetDept()
+        {
+            return "Development";
+        }
+    }
+}
