8f02ffe28693014870c6916ee9958b98b30c2cf4
albert18 <caps18@hotmail.com>
2019-04-02 20:36:49 -0400

    update

---
+++ -Section2-/GetVsPostWebSite/GetVsPostWebSite/GetVsPostWebSite/Controllers/DepartmentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetVsPostWebSite.Controllers
+{
+    //Controller
+    public class DepartmentController : Controller
+    {
+        //Action
+        public string GetDept(int id)
+        {
+            string[] Depts = new string[] {
+                "Testing","Development","HR","QA","Admin"
+            };
+
+            return Depts[id].ToString();
+        }
+
+        public string Index()
+        {
+            return "Manzoor The Trainer - From Index";
+        }
+    }
+}
---
+++ -Section2-/GetVsPostWebSite/GetVsPostWebSite/GetVsPostWebSite/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetVsPostWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        #region Parameterized Method
+        //public ViewResult Save(int DeptId,string DeptName)
+        //{
+        //    //Write Logic to save
+        //    return View();
+        //}
+
+        //public ViewResult GetDept(int DeptId)
+        //{
+        //    //Write Logic to read dept
+        //    return View();
+        //} 
+        #endregion
+
+        public ViewResult Save()
+        {
+            int DeptId = int.Parse(Request.Form["DeptId"].ToString());
+            string DeptName = Request.Form["DeptName"].ToString();
+            //Write Logic to save
+            return View();
+        }
+
+        public ViewResult GetDept()
+        {
+            int DeptId = int.Parse(Request.Query["DeptId"].ToString());
+            //Write Logic to read dept
+            return View();
+        }
+    }
+}
