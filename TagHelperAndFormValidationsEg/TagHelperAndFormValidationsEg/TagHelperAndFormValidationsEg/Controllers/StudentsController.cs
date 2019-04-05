e8aa2703be1f00a33c7a8a9baf477b030d35c5af
albert18 <caps18@hotmail.com>
2019-04-05 08:12:21 -0400

    section 7

---
+++ TagHelperAndFormValidationsEg/TagHelperAndFormValidationsEg/TagHelperAndFormValidationsEg/Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TagHelperAndFormValidationsEg.Data;
+using TagHelperAndFormValidationsEg.Models;
+
+namespace TagHelperAndFormValidationsEg.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student S)
+        {
+            if (ModelState.IsValid)
+            {
+                SchoolDbContext schoolDbContext = new SchoolDbContext();
+                schoolDbContext.Add<Student>(S);
+                schoolDbContext.SaveChanges();
+                return RedirectToAction("Create", "Students");
+            }
+            return View();
+        }
+    }
+}
---
+++ TagHelperAndFormValidationsEg/TagHelperAndFormValidationsEg/TagHelperAndFormValidationsEg/Models/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using TagHelperAndFormValidationsEg.Data;
+
+namespace TagHelperAndFormValidationsEg.Models
+{
+    public class UniqueEmailAttribute:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            SchoolDbContext schoolDbContext = new SchoolDbContext();
+
+            if (value != null)
+            {
+                int count = schoolDbContext.Students.Where(x => x.Email == value.ToString()).Count();
+                if (count == 0)
+                    return ValidationResult.Success;
+                else
+                    return new ValidationResult("This Email already exist!");
+            }
+            else
+            {
+                return new ValidationResult("Email is required!");
+            }
+        }
+    }
+
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage ="User Name is required!")]
+        public string Name { get; set; }
+
+        [Required]        
+        [EmailAddress]
+        [UniqueEmail]
+        public string Email { get; set; }
+
+        //public string Password { get; set; }
+
+        //[Compare("Password")]
+        //public string ConfirmPassword { get; set; }
+
+        //[Range(0,100)]
+        //public double Avg { get; set; }
+
+        //[Url]
+        //public string ProfileUrl { get; set; }
+    }
+}
