e0e4eddebe9aec14e3542aa2096df961f61dd95f
albert18 <caps18@hotmail.com>
2019-04-04 21:22:04 -0400

    Section 6 update

---
+++ ModelBindingAndTempData/ModelBindingAndTempData/Controllers/StudentsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewDataVsViewBagEg.Data;
+using ViewDataVsViewBagEg.Models;
+
+namespace ViewDataVsViewBagEg.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            SchoolDbContext schoolDbContext = new SchoolDbContext();
+            List<Student> students = schoolDbContext.Students.ToList();
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //if (TempData["Msg"] != null)
+            //{
+            //    string Msg = TempData["Msg"].ToString();
+            //    ViewBag.Msg = TempData["Msg"].ToString();
+            //}
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student S)
+        {
+            //string Msg;
+            try
+            {
+                SchoolDbContext schoolDbContext = new SchoolDbContext();
+                schoolDbContext.Add<Student>(S);
+                schoolDbContext.SaveChanges();
+                TempData["Msg"] = "Success!";
+            }
+            catch (Exception E)
+            {
+                TempData["Msg"] = "Failed!";
+            }
+
+            return RedirectToAction("Create", "Students");
+
+        }
+
+        public IActionResult Test()
+        {
+            string Msg = TempData["Msg"].ToString();
+            return View();
+        }
+    }
+}
---
+++ ModelBindingAndTempData/ModelBindingAndTempData/Data/SchoolDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViewDataVsViewBagEg.Models;
+
+namespace ViewDataVsViewBagEg.Data
+{
+    public class SchoolDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-O84LAFN;Database=MVCSchoolDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
