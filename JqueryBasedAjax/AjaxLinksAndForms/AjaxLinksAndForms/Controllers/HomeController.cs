e3c69d0f43883a2ae3dbb6f9edb2c8628e81484f
albert18 <caps18@hotmail.com>
2019-04-17 21:27:27 -0400

    jquery calls

---
+++ JqueryBasedAjax/AjaxLinksAndForms/AjaxLinksAndForms/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxLinksAndForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.MyDate = DateTime.Now.ToString();
+            return View();
+        }
+
+        public PartialViewResult AjaxUpdate()
+        {
+            ViewBag.MyDate = DateTime.Now.ToString();
+            return PartialView("pv_AjaxUpdate");          
+        }
+    }
+}
